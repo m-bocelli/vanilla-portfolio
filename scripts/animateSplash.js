ddaa025fb82de170f47ff3a7649a6deb578eea17
Michael Bocelli <michael.j.bocelli@gmail.com>
2022-08-19 22:37:28 -0400

    Reworked splash animation to be staggered text with persistant presence.

---
+++ scripts/animateSplash.js
@@ -0,0 +1,15 @@
+let nameElement = document.querySelector('.name');
+let buttonElement = document.querySelector('.landing-button');
+
+/* Splash screen is animated when the page fully loads */
+window.addEventListener('DOMContentLoaded', ()=>{
+    let delayTime = 200;
+
+    setTimeout(()=>{
+        /*Name is brought onto screen first*/
+        nameElement.classList.add('active');
+        setTimeout(()=>{
+            buttonElement.classList.add('active');
+        }, delayTime)
+    }, delayTime);
+})
